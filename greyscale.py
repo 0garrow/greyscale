0bbbc4f3b4f87681466c5aa261844048a289cbfb
0garrow <0garrow@protonmail.com>
2024-01-22 18:23:19 +0000

    Implemented filte upload feature.

--- greyscale.py
+++ greyscale.py
@@ -1,17 +1,32 @@
 import streamlit as st
 from PIL import Image
 
-with st.expander("Start Camera"):
-    # Start the camera.
-    camera_image = st.camera_input("Camera")
 
-
-if camera_image:
-    # Create a pollow image instance.
-    img = Image.open(camera_image)
+def convert(image):
+    # Create a pillow image instance.
+    img = Image.open(image)
 
     # Convert the pillow image to greyscale
     grey_img = img.convert("L")
 
     # Render the greyscale image on the webpage
     st.image(grey_img)
+    return
+
+
+st.subheader("Color to Grayscale Converter")
+st.text("Upload Image")
+uploaded_image = st.file_uploader("Upload Image", key="upload")
+
+
+
+with st.expander("Start Camera"):
+    # Start the camera.
+    camera_image = st.camera_input("Camera")
+
+
+if camera_image:
+    convert(camera_image)
+
+if uploaded_image:
+    convert(uploaded_image)
